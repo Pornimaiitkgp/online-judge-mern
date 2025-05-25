350eaa4f2d9f8062d186cae7c69fe1c7c27c9ae4
Pornima Gaikwad <pornimagaikwad2003@gmail.com>
2025-05-26 02:52:27 +0530

    complete signup page functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,11 @@
-import User from '../models/User.js';
-import bcrypt from 'bcryptjs';
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
 
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = newUsesr({username, email, password: hashedPassword}); 
+    const newUser = new User({username, email, password: hashedPassword}); 
     try {
     await newUser.save()
     res.status(201).json('User created successfully');
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
+
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB');
 }
@@ -13,10 +14,11 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.error(err);
 }
 );
+const app = express();
 
 app.use(express.json());
 
-const app = express();  
+ 
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
