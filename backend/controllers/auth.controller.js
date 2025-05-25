e0ddc430fe80504a0cf77a17739e7b45b35c46c2
Pornima Gaikwad <pornimagaikwad2003@gmail.com>
2025-05-26 01:12:21 +0530

    create signup api route

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+
+
+export const signup = async (req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = newUsesr({username, email, password: hashedPassword}); 
+    try {
+    await newUser.save()
+    res.status(201).json('User created successfully');
+    }
+    catch (error) {
+        res.status(500).json( error.message );
+    }
+
+};
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';  
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -12,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 }
 );
 
+app.use(express.json());
+
 const app = express();  
 
 app.listen(3000, () => {
@@ -19,4 +23,5 @@ app.listen(3000, () => {
     }
 );
 
-app.use("/api/user", userRouter)
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter);
