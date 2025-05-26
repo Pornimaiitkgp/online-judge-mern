9d751f220c2153a68a54a9834ac130a2234bf8c4
Pornima Gaikwad <pornimagaikwad2003@gmail.com>
2025-05-26 14:12:00 +0530

    create sign in api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -15,3 +16,18 @@ export const signup = async (req, res, next) => {
     }
 
 };
+
+export const signin = async(req, res,next) => {
+    const { email, password } = req.body;
+    try {
+        const ValidUser = await User.findOne({ email }); 
+        if (!ValidUser)  return next(errorHandler(404,  'User not found') ); 
+        const ValidUserPassword = bcryptjs.compareSync(password, ValidUser.password);
+        if (!ValidUserPassword) return next(errorHandler(400, 'Wrong Credentials'));
+        const token = jwt.sign({ id: ValidUser._id }, process.env.JWT_SECRET);
+        const {password: pass, ...rest}= validUser._doc; // Exclude password from the response
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(valudUser);
+     } catch (error) {
+        next(error);
+    }
+}
