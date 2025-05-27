693bdc48797e1af090dfd6192f1b525138a85368
Pornima Gaikwad <pornimagaikwad2003@gmail.com>
2025-05-28 03:58:02 +0530

    complete update profile functionality

--- frontend/src/redux/user/userSlice.js
+++ frontend/src/redux/user/userSlice.js
@@ -7,7 +7,8 @@ const initialState = {
 };
 
 const userSlice = createSlice({
-  name: 'user',
+  
+    name: 'user',
   initialState,
   reducers: {
     signInStart: (state) => {
@@ -34,30 +35,6 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
-    deleteUserStart: (state) => {
-      state.loading = true;
-    },
-    deleteUserSuccess: (state) => {
-      state.currentUser = null;
-      state.loading = false;
-      state.error = null;
-    },
-    deleteUserFailure: (state, action) => {
-      state.error = action.payload;
-      state.loading = false;
-    },
-    signOutUserStart: (state) => {
-      state.loading = true;
-    },
-    signOutUserSuccess: (state) => {
-      state.currentUser = null;
-      state.loading = false;
-      state.error = null;
-    },
-    signOutUserFailure: (state, action) => {
-      state.error = action.payload;
-      state.loading = false;
-    },
   },
 });
 
@@ -68,12 +45,6 @@ export const {
   updateUserFailure,
   updateUserSuccess,
   updateUserStart,
-  deleteUserFailure,
-  deleteUserSuccess,
-  deleteUserStart,
-  signOutUserFailure,
-  signOutUserSuccess,
-  signOutUserStart,
 } = userSlice.actions;
 
 export default userSlice.reducer;
