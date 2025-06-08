f93bc15249fd5f8e3fd42016be9951b4dd154f65
Pornima Gaikwad <pornimagaikwad2003@gmail.com>
2025-06-08 15:23:40 +0530

    ad

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
   'ojudge-fa3bc.firebaseapp.com',
   'ojudge-fa3bc.web.app',
-  'https://oj-frontend-5ppu96ici-pornima-gaikwads-projects.vercel.app'
+  'https://oj-frontend-glcwkgpbw-pornima-gaikwads-projects.vercel.app'
 ];
 
 app.use(cors({
