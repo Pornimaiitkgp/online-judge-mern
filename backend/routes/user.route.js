a6c1650058a8bb9c0d6cfe0ee88021d5d8a8e166
Pornima Gaikwad <pornimagaikwad2003@gmail.com>
2025-06-11 12:34:03 +0530

    sf

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,7 +3,7 @@ import { deleteUser, test, updateUser } from '../controllers/user.controller.js'
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();  
-router.post('/update/:id', verifyToken, updateUser);  
+router.put('/update/:id', verifyToken, updateUser);  
 router.delete('/delete/:id', verifyToken, deleteUser)
 
 
