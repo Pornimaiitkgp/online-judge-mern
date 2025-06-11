610be0d46fc691c80569d5d5010c30f84642f176
Pornima Gaikwad <pornimagaikwad2003@gmail.com>
2025-06-11 11:42:35 +0530

    tww

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ const allowedOrigins = [
   process.env.FRONTEND_URL.split(','),
   'ojudge-fa3bc.firebaseapp.com',
   'ojudge-fa3bc.web.app',
-  'https://oj-frontend-avz3wwflh-pornima-gaikwads-projects.vercel.app',
+  'https://oj-frontend-2mn75l8hz-pornima-gaikwads-projects.vercel.app',
   'https://oj-frontend-psi.vercel.app'
 ];
 
